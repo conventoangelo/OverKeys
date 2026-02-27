af213bc894ff883f3df40134cd30b6e95a16a2fd
Angelo Convento <convento.angelo@gmail.com>
2026-02-28 00:29:01 +0800

    feat: add QWERTY (Foreign) keyboard layout

--- lib/models/keyboard_layouts.dart
+++ lib/models/keyboard_layouts.dart
@@ -265,6 +265,17 @@ const greek = KeyboardLayout(
     ],
     foreign: true);
 
+const qwertyForeign = KeyboardLayout(
+  name: 'QWERTY (Foreign)',
+  keys: [
+    ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "BSPC"],
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'"],
+    ['Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/'],
+    [' '],
+  ],
+);
+
 final List<KeyboardLayout> availableLayouts = [
   qwerty,
   colemak,
