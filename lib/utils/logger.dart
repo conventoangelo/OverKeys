7c6a99871115d97b0d9243864fd91baeea13fa89
Angelo Convento <convento.angelo@gmail.com>
2026-02-28 13:22:51 +0800

    feat: implement SimplePrintLogger with timestamped log levels

---
+++ lib/utils/logger.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/foundation.dart';
+
+/// Simple print-based logger with timestamps and log levels
+class SimplePrintLogger {
+  final String name;
+
+  SimplePrintLogger(this.name);
+
+  String _timestamp() => DateTime.now().toString().substring(11, 23);
+
+  void debug(String message) {
+    if (kDebugMode) {
+      print('[${_timestamp()}] [$name] $message');
+    }
+  }
+
+  void info(String message) {
+    if (kDebugMode) {
+      print('[${_timestamp()}] [$name] ℹ️ $message');
+    }
+  }
+
+  void warning(String message, {Object? error, StackTrace? stackTrace}) {
+    if (kDebugMode) {
+      print('[${_timestamp()}] [$name] ⚠️ $message');
+      if (error != null) print('Error: $error');
+      if (stackTrace != null) print('StackTrace: $stackTrace');
+    }
+  }
+
+  void error(String message, {Object? error, StackTrace? stackTrace}) {
+    if (kDebugMode) {
+      print('[${_timestamp()}] [$name] ❌ $message');
+      if (error != null) print('Error: $error');
+      if (stackTrace != null) print('StackTrace: $stackTrace');
+    }
+  }
+}
